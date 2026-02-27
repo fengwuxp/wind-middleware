48e2cdcdd8356e49a3cef2a6e1fe2b8b8147efbf
wuxp <1109695647@qq.com>
2026-02-27 14:50:53 +0800

    chore(elasticjob): 添加临时兼容性类解决依赖冲突
    - 添加 DataSourceAutoConfiguration 作为临时 hack
    - 添加 DataSourceProperties 类以兼容 ElasticJob 的 TracingProperties
    - 标记新添加的类为 @Deprecated(forRemoval = true)

---
+++ wind-elasticjob/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceAutoConfiguration.java
@@ -0,0 +1,11 @@
+package org.springframework.boot.autoconfigure.jdbc;
+
+/**
+ * TODO 临时 hack
+ * @author wuxp
+ * @date 2026-02-27 14:27
+ * @see org.apache.shardingsphere.elasticjob.lite.spring.boot.job.ElasticJobLiteAutoConfiguration
+ **/
+@Deprecated(forRemoval = true)
+public class DataSourceAutoConfiguration {
+}
---
+++ wind-elasticjob/src/main/java/org/springframework/boot/autoconfigure/jdbc/DataSourceProperties.java
@@ -0,0 +1,13 @@
+package org.springframework.boot.autoconfigure.jdbc;
+
+
+/**
+ * TODO 临时 hack 为了编译通过
+ * 兼容 elasticjob {@link org.apache.shardingsphere.elasticjob.lite.spring.boot.tracing.TracingProperties}
+ *
+ * @author wuxp
+ * @date 2026-02-27 14:24
+ **/
+@Deprecated(forRemoval = true)
+public class DataSourceProperties {
+}

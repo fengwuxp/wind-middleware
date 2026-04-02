3adb1f3433b7ec510c91bd341e9a5611daf260a3
wuxp <1109695647@qq.com>
2026-04-02 10:58:03 +0800

    fix(tests): 删除过时的测试用例

--- wind-tracer/src/test/java/com/wind/trace/WindThreadTracerTests.java
+++ wind-tracer/src/test/java/com/wind/trace/WindThreadTracerTests.java
@@ -1,116 +1,18 @@
 package com.wind.trace;
 
 import com.wind.common.WindConstants;
-import com.wind.trace.task.ContextPropagationTaskDecorator;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.slf4j.MDC;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Objects;
-import java.util.concurrent.CountDownLatch;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import static com.wind.common.WindConstants.LOCALHOST_IP_V4;
-import static com.wind.common.WindConstants.TRACE_ID_NAME;
 
 /**
  * @author wuxp
  * @date 2025-02-25 10:34
  **/
 class WindThreadTracerTests {
-
-    @Test
-    void testGetTranceId() {
-        Assertions.assertNotNull(WindTracer.TRACER.getContextVariables());
-        String traceId = WindTracer.TRACER.getTraceId();
-        Assertions.assertNotNull(traceId);
-        WindTracer.TRACER.clear();
-        WindTracer.TRACER.trace();
-        Assertions.assertNotEquals(traceId, WindTracer.TRACER.getTraceId());
-        Assertions.assertNotNull(WindTracer.TRACER.getContextVariable(LOCALHOST_IP_V4));
-    }
-
-    @Test
-    void testTrace() {
-        String traceId = "test";
-        WindTracer.TRACER.trace(traceId);
-        Assertions.assertEquals(traceId, WindTracer.TRACER.getTraceId());
-        WindTracer.TRACER.trace("2");
-        Assertions.assertEquals("2", WindTracer.TRACER.getTraceId());
-    }
-
-    @Test
-    void testTraceVariables() {
-        HashMap<String, Object> contextVariables = new HashMap<>();
-        contextVariables.put("key1", "1");
-        contextVariables.put("key2", 2);
-        contextVariables.put("key3", null);
-        WindTracer.TRACER.trace("test001", contextVariables);
-        Assertions.assertEquals("1", WindTracer.TRACER.getContextVariable("key1"));
-        Assertions.assertEquals(2, (Integer) WindTracer.TRACER.getContextVariable("key2"));
-        WindTracer.TRACER.putVariable("key3", false);
-        Assertions.assertEquals(false, Objects.requireNonNull(WindTracer.TRACER.getContextVariable("key3")));
-    }
-
-    @Test
-    void testTraceClear() {
-        String traceId = "test";
-        WindTracer.TRACER.trace(traceId);
-        Assertions.assertEquals(traceId, WindTracer.TRACER.getTraceId());
-        WindTracer.TRACER.clear();
-        Assertions.assertNotEquals(traceId, WindTracer.TRACER.getTraceId());
-        WindTracer.TRACER.clear();
-    }
-
-    @Test
-    void testTraceNewThreadCopy() throws Exception {
-        Map<String, Object> contextVariables = new HashMap<>();
-        contextVariables.put("a", "test");
-        WindTracer.TRACER.trace(RandomStringUtils.secure().nextAlphabetic(32), contextVariables);
-        String traceId = WindTracer.TRACER.getTraceId();
-        Assertions.assertEquals(traceId, MDC.get(TRACE_ID_NAME));
-        CountDownLatch downLatch = new CountDownLatch(1);
-        try (ExecutorService executorService = Executors.newSingleThreadExecutor()) {
-            executorService.execute(ContextPropagationTaskDecorator.of().decorate(() -> {
-                try {
-                    String actual = WindTracer.TRACER.getTraceId();
-                    Assertions.assertEquals(traceId, actual);
-                    Assertions.assertEquals(traceId, MDC.get(TRACE_ID_NAME));
-                    Assertions.assertEquals("test", WindTracer.TRACER.requireContextVariable("a"));
-                    Assertions.assertEquals("test", MDC.get("a"));
-                } finally {
-                    downLatch.countDown();
-                }
-            }));
-            downLatch.await();
-            Assertions.assertEquals(traceId, WindTracer.TRACER.getTraceId());
-        }
-    }
-
-    @Test
-    void testTraceNewThreadNoCopy() throws Exception {
-        String traceId = WindTracer.TRACER.getTraceId();
-        Assertions.assertEquals(traceId, MDC.get(TRACE_ID_NAME));
-        CountDownLatch downLatch = new CountDownLatch(1);
-        try (ExecutorService executorService = Executors.newSingleThreadExecutor()) {
-            executorService.execute(() -> {
-                try {
-                    WindTracer.TRACER.trace();
-                    Assertions.assertNotEquals(traceId, WindTracer.TRACER.getTraceId());
-                    Assertions.assertNotEquals(traceId, MDC.get(TRACE_ID_NAME));
-                } finally {
-                    WindTracer.TRACER.clear();
-                    downLatch.countDown();
-                }
-            });
-            downLatch.await();
-            Assertions.assertEquals(traceId, WindTracer.TRACER.getTraceId());
-        }
-    }
 
     @Test
     void testRemoveVariable() {
@@ -148,7 +50,7 @@ class WindThreadTracerTests {
         WindTracer.TRACER.run(() -> {
             String spanId = WindTracer.TRACER.requireContext().spanId();
             WindTracer.TRACER.runWithContext(WindTracer.TRACER.requireContext(), () -> {
-                Assertions.assertEquals(spanId,  WindTracer.TRACER.requireContext().parentSpanId());
+                Assertions.assertEquals(spanId, WindTracer.TRACER.requireContext().parentSpanId());
             });
         });
     }
